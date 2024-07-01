d8aec311811d171819c0bc387cf93aa99be1dd4a
yashseth391 <115554024+yashseth391@users.noreply.github.com>
2024-07-02 00:41:30 +0530

    Adding musics

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar/>
+      <Text>Testing</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
