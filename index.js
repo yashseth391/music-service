03b2a8660758629f68635cfd6794536fe00231b8
yashseth391 <115554024+yashseth391@users.noreply.github.com>
2024-07-02 23:54:45 +0530

    added our music service

--- index.js
+++ index.js
@@ -5,5 +5,8 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import { playBackService } from './musicService';
+
 
 AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() => require('./service'));
